e8e69d64e4696dec17afeb0935a75b5b870728a3
CharanSeri <seri0009@algonquinlive.com>
2024-09-09 21:36:09 -0400

    added transition observer to certificatesDiv

--- main.js
+++ main.js
@@ -1,13 +1,26 @@
-
-
 document.getElementById("cardsContainer").onmousemove = e => {
     for (const card of document.querySelectorAll(".card")) {
         const rect = card.getBoundingClientRect(),
             x = e.clientX - rect.left,
             y = e.clientY - rect.top;
 
-            card.style.setProperty("--mouse-x", `${x}px`);
-            card.style.setProperty("--mouse-y", `${y}px`);
-        
+        card.style.setProperty("--mouse-x", `${x}px`);
+        card.style.setProperty("--mouse-y", `${y}px`);
+
     }
 }
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show')
+        } else {
+            entry.target.classList.remove('show')
+        }
+    })
+})
+
+const certificateContainers = document.querySelectorAll('.certificateDivHidden');
+certificateContainers.forEach((el) =>
+    observer.observe(el)
+)
